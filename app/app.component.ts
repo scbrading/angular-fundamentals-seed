28e5ecd740050ce320defbf69a5c415836a004f8
Clark Brading <scbrading@gmail.com>
2019-04-18 16:54:46 -0400

    Created stateless dashboard components.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,77 +1,15 @@
 import { Component } from '@angular/core';
 
-interface Passenger {
-  id: number;
-  fullname: string;
-  checkedIn: boolean;
-}
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div class="app">
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span class="status" [class.checked-in]="passenger.checkedIn"></span>
-          {{ i }}: {{ passenger.fullname }}
-        </li>
-      </ul>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [ngClass]="{
-              'checked-in': passenger.checkedIn,
-              'checked-out': !passenger.checkedIn
-            }"
-          ></span>
-          {{ i }}: {{ passenger.fullname }}
-        </li>
-      </ul>
-      <h3>Airline Passengers</h3>
-      <ul>
-        <li *ngFor="let passenger of passengers; let i = index">
-          <span
-            class="status"
-            [ngStyle]="
-              {backgroundColor: passenger.checkedIn ? '#2ecc71' : '#c0392b'}
-            "
-          ></span>
-          {{ i }}: {{ passenger.fullname }}
-        </li>
-      </ul>
+      <passenger-dashboard></passenger-dashboard>
     </div>
   `,
 })
 export class AppComponent {
-  passengers: Passenger[] = [
-    {
-      id: 1,
-      fullname: 'Stephen',
-      checkedIn: true,
-    },
-    {
-      id: 2,
-      fullname: 'Rose',
-      checkedIn: false,
-    },
-    {
-      id: 3,
-      fullname: 'James',
-      checkedIn: true,
-    },
-    {
-      id: 4,
-      fullname: 'Louise',
-      checkedIn: true,
-    },
-    {
-      id: 5,
-      fullname: 'Tina',
-      checkedIn: false,
-    },
-  ];
+
 }
