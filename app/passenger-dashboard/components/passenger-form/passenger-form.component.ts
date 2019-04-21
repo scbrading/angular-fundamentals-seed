3e73c41760adab6fbd3cb3c5af54c3573a976ad2
Clark Brading <scbrading@gmail.com>
2019-04-21 19:48:46 -0400

    forms complete.

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Passenger } from '../../models/passenger.interface';
+import { Baggage } from '../../models/baggage.interface';
 
 @Component({
   selector: 'passenger-form',
   styleUrls: ['passenger-form.component.scss'],
   template: `
-    <form #form="ngForm" novalidate>
+    <form
+      (ngSubmit)="handleSubmit(form.value, form.valid)"
+      #form="ngForm"
+      novalidate
+    >
       {{ detail | json }}
       <div>
         Passenger name:
@@ -13,8 +18,13 @@ import { Passenger } from '../../models/passenger.interface';
           type="text"
           [ngModel]="detail?.fullname"
           name="fullname"
+          #fullname="ngModel"
+          required
           autocomplete="off"
         />
+        <div *ngIf="fullname.errors?.required && fullname.dirty" class="error">
+          Passenger name is required
+        </div>
       </div>
       <div>
         Passenger ID:
@@ -22,8 +32,13 @@ import { Passenger } from '../../models/passenger.interface';
           type="number"
           [ngModel]="detail?.id"
           name="id"
+          #id="ngModel"
+          required
           autocomplete="off"
         />
+        <div *ngIf="id.errors?.required && id.dirty" class="error">
+          Passenger name is required
+        </div>
       </div>
       <div>
         <label>
@@ -45,14 +60,59 @@ import { Passenger } from '../../models/passenger.interface';
           [ngModel]="detail?.checkInDate"
         />
       </div>
+      <div>
+        Luggage:
+        <select name="baggage" [ngModel]="detail?.baggage">
+          <option
+            *ngFor="let item of baggage"
+            [value]="item.key"
+            [selected]="item.key === detail?.baggage"
+          >
+            {{ item.value }}
+          </option>
+        </select>
+        <select name="baggage" [ngModel]="detail?.baggage">
+          <option *ngFor="let item of baggage" [ngValue]="item.key">
+            {{ item.value }}
+          </option>
+        </select>
+      </div>
 
-      {{ form.value | json }}
+      <button type="submit" [disabled]="form.invalid">Update passenger</button>
     </form>
   `,
 })
 export class PassengerFormComponent {
   @Input()
   detail: Passenger;
+
+  @Output()
+  update: EventEmitter<Passenger> = new EventEmitter<Passenger>();
+
+  baggage: Baggage[] = [
+    {
+      key: 'none',
+      value: 'No baggage',
+    },
+    {
+      key: 'hand-only',
+      value: 'Hand baggage',
+    },
+    {
+      key: 'hold-only',
+      value: 'Hold baggage',
+    },
+    {
+      key: 'hand-hold',
+      value: 'Hand and hold baggage',
+    },
+  ];
+
+  handleSubmit(passenger: Passenger, isValid: boolean) {
+    if (isValid) {
+      this.update.emit(passenger);
+    }
+  }
 
   toggleCheckIn(checkedIn: boolean) {
     if (checkedIn) {
--- app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
+++ app/passenger-dashboard/containers/passenger-viewer/passenger-viewer.component.ts
@@ -7,7 +7,10 @@ import { Passenger } from '../../models/passenger.interface';
   styleUrls: ['passenger-viewer.component.scss'],
   template: `
     <div>
-      <passenger-form [detail]="passenger"></passenger-form>
+      <passenger-form
+        [detail]="passenger"
+        (update)="onUpdatePassenger($event)"
+      ></passenger-form>
     </div>
   `,
 })
@@ -19,5 +22,13 @@ export class PassengerViewerComponent implements OnInit {
     this.passengerService
       .getPassenger(1)
       .subscribe((data: Passenger) => (this.passenger = data));
+  }
+
+  onUpdatePassenger(event: Passenger) {
+    this.passengerService
+      .updatePassenger(event)
+      .subscribe((data: Passenger) => {
+        this.passenger = Object.assign({}, this.passenger, event);
+      });
   }
 }
